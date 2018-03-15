2b831e3654bac3e2bbb964225054d2d09f3bc047
yusheng <yusheng.liu@lianbi.com.cn>
2018-03-15 11:19:19 +0800

    Initial Commit

---
+++ Vistor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vistor
+//
+//  Created by yusheng on 2018/3/15.
+//  Copyright © 2018年 yusheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
